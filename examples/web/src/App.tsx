643f729e33e8fa36719a18ced1fc4cf184ed5970
sbin <sbinha123@gmail.com>
2024-02-16 09:02:48 +0900

    wip: examples

---
+++ examples/web/src/App.tsx
@@ -0,0 +1,48 @@
+import { createGlobalStyle } from 'styled-components'
+import reset from 'styled-reset'
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0; 
+    padding: 0;
+    background-color: #000;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  a{
+    text-decoration: none;
+    color: inherit;
+  }
+  html, body, div, span, h1, h2, h3, h4, h5, h6, p, 
+  a, dl, dt, dd, ol, ul, li, form, label, table{
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 10px;
+    vertical-align: baseline;
+  }
+  ol, ul{
+    list-style: none;
+  }
+  button {
+    border: 0;
+    background: transparent;
+    cursor: pointer;
+  }
+`
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+    </>
+  )
+}
+
+export default App
